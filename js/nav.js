9ef1e8a579f815b36613e3cf57a78a77e91e93f8
xiaobo hu <xiaobo.xiao@gmail.com>
2016-07-18 01:14:10 -0700

    added footer, too tired to test the footer css

--- js/nav.js
+++ js/nav.js
@@ -2,28 +2,32 @@
 var nav = angular.module('navApp',[]);
 nav.controller('myNav',['$scope', '$window', '$location', function($scope, $window, $location){
     //initalizes the page specific nav bar on load
+    /*
     var initNav = function(){
         //creates a list of that we will be navigating
-        $scope.navList = ["Home", "Crime Level", "Recent Crime", "Safety Tips"];
+        $scope.navList = [{name:"Home", link:"home"}, {name:"Crime Level", link:"crimelevel"}, {name:"Recent Crime", link:"list"}, {name:"Safety Tips", link:"safetytips"}];
         $scope.navContent = {};
+        console.log($scope.navList);
         var removeIndex = 0;
         if(window.location.href.indexOf("#/home") != -1)
             removeIndex = 0;
         else if(window.location.href.indexOf("#/home/crimelevel"))
             removeIndex = 1;
-        else if(window.location.href.indexOf("#/home/recentcrime"))
+        else if(window.location.href.indexOf("#/list"))
             removeIndex = 2;
         else if(window.location.href.indexOf("#/home/safetytips"))
             removeIndex = 3;
         $scope.navList.splice(removeIndex, 1);
-        $scope.navContent.first = $scope.navList[0];
-        $scope.navContent.second = $scope.navList[1];
-        $scope.navContent.third = $scope.navList[2];
+        $scope.navContent.first = $scope.navList[0].name;
+        $scope.navContent.second = $scope.navList[1].name;
+        $scope.navContent.third = $scope.navList[2].name;
+        $scope.navContent.firstL = $scope.navList[0].link;
+        $scope.navContent.secondL = $scope.navList[1].link;
+        $scope.navContent.thirdL = $scope.navList[2].link;
     }
-    
-    var makeLink = function(content){
-      var str = content.replace(/\s+/g, '');
-      return str.toLowerCase();
-    }
-    initNav();
+    initNav();*/
+
+    $scope.isActive = function (viewLocation) { 
+        return !(viewLocation === $location.path());
+    };
 }]);
